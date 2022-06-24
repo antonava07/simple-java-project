0c81db46f834623bbe221983033c8db82cb659ae
Antonia <anto.nava07@gmail.com>
2022-06-24 15:01:21 -0400

    segundo commit

--- ProyectoDePrueba/src/test/java/com/proyectos/ProyectoDePrueba/AppTest.java
+++ ProyectoDePrueba/src/test/java/com/proyectos/ProyectoDePrueba/AppTest.java
@@ -37,12 +37,12 @@ public class AppTest
         int n=69;
         assertEquals("Esta dentro de la poblacion ADULTA MAYOR",App.categoria(n));
     }
-    
+    /*
     @Test
     public void edadInvalida()
     {
         int n=-1;
         assertEquals("ERROR: Ingrese una edad valida.",App.categoria(n));
     }
-
+	*/
 }
